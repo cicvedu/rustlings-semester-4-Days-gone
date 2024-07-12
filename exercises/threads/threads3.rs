88dfd8cf378b1de1673f371c1ecba6f1c573a7c1
xym <1328699220@qq.com>
2024-07-12 16:35:32 +0800

    update

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::sync::mpsc;
 use std::sync::Arc;
@@ -27,6 +26,12 @@ impl Queue {
 }
 
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
+    // cannot directly use the reference in the closure, will cause the 
+    // move semantics to move the variable into the closure, and the second
+    // closure will not be able to use it. So we clone the sender outside the 
+    // closure and use the clone inside the closure.
+    let tx_clone1 = mpsc::Sender::clone(&tx);
+    let tx_clone2 = mpsc::Sender::clone(&tx);
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
@@ -34,7 +39,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx_clone1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -42,7 +47,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx_clone2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
